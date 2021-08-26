b2b1f32967b64d62190f4b493cb2b0e6f59285ca
BoiseState Student <student@u.boisestate.edu>
2021-08-26 15:39:52 -0600

    Activity 3 Completed

---
+++ A3-PersonalBio/PersonalBio.java
@@ -0,0 +1,11 @@
+public class PersonalBio 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Name: Jordan Ackley");
+        System.out.println("Birthday: December 21, 2001");
+        System.out.println("Hobbies: Video Games, Hiking, Physics");
+        System.out.println("Favorite Book: Probably Night by Elie Wiesel");
+        System.out.println("Favorite Movie: Probably The Dark Knight");       
+    }
+}
